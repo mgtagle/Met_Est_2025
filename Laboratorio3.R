5966bd3bf9a4cd36a7c7efb29528136216ae6c7d
Marco <marco.gonzaleztg@uanl.edu.mx>
2025-08-20 08:25:01 -0600

    Update

---
+++ Laboratorio3.R
@@ -0,0 +1,20 @@
+# Laboratorio Semana 3
+# 20/08/2025
+# Marco A González tagle
+
+
+# Importar datos ----------------------------------------------------------
+
+Temp <- read.csv("temperatura.csv", header = T)
+Temp <- read.csv("Data/Medias_temp.csv", header = T)
+# Graficar datos ----------------------------------------------------------
+
+
+
+# Estadísticas descriptivas -----------------------------------------------
+
+
+
+# Estadística inferencial -------------------------------------------------
+
+
